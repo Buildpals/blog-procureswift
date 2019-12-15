e793717122f3cd969d6824b25a770256ff6cb570
bkwaku <kbboateng14@gmail.com>
2019-12-15 17:10:11 +0000

    Add subtitle to post.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_154757) do
+ActiveRecord::Schema.define(version: 2019_12_15_170705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2019_12_15_154757) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "draft", default: true
     t.integer "catergory"
+    t.string "sub_title"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
